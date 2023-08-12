dd3b4fcbb76b1a788ae3dc320085e7c7f9c9af08
DHughes666 <graziemolto6@gmail.com>
2023-08-12 19:45:01 +0100

    Issues with update in mongoose

--- app.js
+++ app.js
@@ -23,18 +23,78 @@ const articleSchema = new mongoose.Schema ({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
-  Article.find()
-  .then(data => res.send(data))
-  .catch(err => res.send(err))
-})
 
-// python = new Article({
-//   title: "Python",
-//   content: "The dopest of all programming languages I know"
+// // Get request (Fetches data)
+// app.get("/articles", function(req, res){
+//   Article.find()
+//   .then(data => res.send(data))
+//   .catch(err => res.send(err))
 // });
 //
-// python.save().then(data => console.log(data))
+// // Post request
+// app.post("/articles", function(req, res){
+//
+//   const newArticle = new Article({
+//     title: req.body.title,
+//     content: req.body.content
+//   });
+//   newArticle.save()
+//   .then(data => res.send("Successfully added a new article."))
+//   .catch(err => res.send(err))
+// });
+//
+// app.delete("/articles", function(req, res){
+//   Article.deleteMany()
+//   .then(data => res.send("Collections Successfully emptied!"))
+//   .catch(err => res.send(err));
+// })
+
+//Refactoring
+app.route("/articles")
+  .get(function(req, res){
+    Article.find()
+    .then(data => res.send(data))
+    .catch(err => res.send(err))
+  })
+  .post(function(req, res){
+      const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+      });
+      newArticle.save()
+      .then(data => res.send("Successfully added a new article."))
+      .catch(err => res.send(err))
+    })
+  .delete(function(req, res){
+      Article.deleteMany()
+      .then(data => res.send("Collections Successfully emptied!"))
+      .catch(err => res.send(err));
+    });
+
+
+  //Working on specific articles
+  app.route("/articles/:specificTitle")
+    .get(function(req, res){
+      const specificTitle = req.params.specificTitle;
+
+      Article.findOne({title: specificTitle})
+      .then(data => res.send(data))
+      .catch(err => res.send(err))
+    })
+    .post(function(req, res){
+      const specificTitle = req.params.specificTitle;
+        Article.findOneAndUpdate(
+          {name: specificTitle}, {content:req.body.content}, {new: true})
+        .then(data => res.send(`${specificTitle} updated`))
+        .catch(err => res.send(err))
+      })
+    .delete(function(req, res){
+        Article.deleteMany()
+        .then(data => res.send("Collections Successfully emptied!"))
+        .catch(err => res.send(err));
+      });
+
+
 
 
 
